bb6fd583c8b95b52c97ef529ce0ad65f3b83c07d
Hanna Nabil <hananabil@live.com>
2018-12-17 00:05:33 +0200

    Add files via upload

---
+++ calculator.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+uname
+#echo "Hello world"
+#echo  "Hello ,$USER"
+#read -p "Enter You name " user_name  
+#echo  "hello $user_name "
+read -p "Enter Number " x
+read -p "Enter Operation " o 
+read -p "Enter Number " y
+echo "Result= " $[$x $o $y]
